8dc41eea81e12ada3977f22fabc456361adde5f6
Shankar Ganesh Purushothaman <puru.shankarg@gmail.com>
2022-05-29 09:35:24 +0530

    Add files via upload

---
+++ College.java
@@ -0,0 +1,19 @@
+package org.college;
+
+public class College {
+	
+	public void collegeName() 
+	{
+		System.out.println("Name of the college is Muthurangam");
+	}
+	public void collegecode() 
+	{
+		System.out.println("College code is MAC007");
+	}
+	public void collegeRank() 
+	{
+		System.out.println("First Rank");
+	}
+
+
+}
